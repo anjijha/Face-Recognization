10ac8eb80372554976e98cddeadad733b33bf11b
anjalijha301@gmail.com <anjalijha301@gmail.com>
2023-04-15 18:12:44 +0530

    front page upper 3 images done

--- main.py
+++ main.py
@@ -9,13 +9,27 @@ class Face_Recognition_System:
         self.root.geometry("1530x790+0+0")
         self.root.title("face Recognition System")
 
-
+#first image
         img = Image.open("college_images/Stanford.jpg ")
-        img=img.resize((500, 130),Image.ANTIALIAS)
+        img=img.resize((500, 130),Image.Resampling.LANCZOS)
         self.photoimg=ImageTk.PhotoImage(img)
 
         f_lbl=Label(self.root,image=self.photoimg)
         f_lbl.place(x=0,y=0,width=500,height=130)
+#second image
+        img1 = Image.open("college_images/facialrecognition.png ")
+        img1=img1.resize((500, 130),Image.Resampling.LANCZOS)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        f_lbl=Label(self.root,image=self.photoimg1)
+        f_lbl.place(x=500,y=0,width=500,height=130) 
+#third image
+        img2 = Image.open("college_images/u.jpg ")
+        img2=img2.resize((500, 130),Image.Resampling.LANCZOS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        f_lbl=Label(self.root,image=self.photoimg2)
+        f_lbl.place(x=1000,y=0,width=500,height=130) 
 
 
 if __name__ == "__main__":
