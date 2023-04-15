e4133b1f052f50eb20946aa996baed7fada779e8
anjalijha301@gmail.com <anjalijha301@gmail.com>
2023-04-15 17:36:39 +0530

    Main File of Code

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image,ImageTk
+
+
+class Face_Recognition_System:
+    def __init__(self, root):
+        self.root=root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("face Recognition System")
+
+
+        img = Image.open("college_images/Stanford.jpg ")
+        img=img.resize((500, 130),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img)
+
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=500,height=130)
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=Face_Recognition_System(root)
+    root.mainloop()
+
+        
+ 
