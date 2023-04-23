5ee3e4f734b8bd515460eda6e235f8f1389b23b6
anjalijha301@gmail.com <anjalijha301@gmail.com>
2023-04-23 18:40:46 +0530

    images and button is done

--- face_recognition.py
+++ face_recognition.py
@@ -14,9 +14,32 @@ class Face_Recognition:
         self.root.geometry("1530x790+0+0")
         self.root.title("face Recognition System")
 
+        title_lb1=Label(self.root,text="FACE RECOGNITION",font=("times new romain",35,"bold"),bg="white",fg="darkgreen")
+        title_lb1.place(x=0, y=0, width=1530,height=45)
 
+        #=========== 1images ===========
+
+        img_top = Image.open("college_images/face_detector1.jpg")
+        img_top=img_top.resize((650, 700),Image.Resampling.LANCZOS)
+        self.photoimg_top=ImageTk.PhotoImage(img_top)
+
+        f_lbl=Label(self.root,image=self.photoimg_top)
+        f_lbl.place(x=0,y=55,width=650,height=700) 
+
+        #==============2 images==========
+        img_bottom = Image.open("college_images/detector5.jpg")
+        img_bottom=img_bottom.resize((950, 700),Image.Resampling.LANCZOS)
+        self.photoimg_bottom=ImageTk.PhotoImage(img_bottom)
+
+        f_lbl=Label(self.root,image=self.photoimg_bottom)
+        f_lbl.place(x=650,y=55,width=950,height=700)
+
+         #================Button=============
+
+        b1_1=Button(f_lbl,text="Face Recognition", cursor="hand2", font=("times new roman",18,"bold"),bg="darkgreen", fg="white")
+        b1_1.place(x=365,y=620,width=200,height=40) 
 
 if __name__ == "__main__":
       root=Tk()
       obj=Face_Recognition(root)
-      root.mainloop()        
+      root.mainloop()         
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image,ImageTk
 import os
 from student import Student
 from train import Train
+from face_recognition import Face_Recognition
 
 
 
@@ -61,12 +62,12 @@ class Face_Recognition_System:
         img5=img5.resize((220, 220),Image.Resampling.LANCZOS)
         self.photoimg5=ImageTk.PhotoImage(img5)
 
-        b1=Button(self.root,image=self.photoimg5, cursor="hand2")
+        b1=Button(self.root,image=self.photoimg5, cursor="hand2",command=self.face_data)
 
         b1.place(x=490,y=220,width=220,height=220) 
         
 
-        b1_1=Button(bg_img,text="Face Detector", cursor="hand2", font=("times new roman",15,"bold"),bg="darkblue", fg="white")
+        b1_1=Button(bg_img,text="Face Detector", cursor="hand2",command=self.face_data,font=("times new roman",15,"bold"),bg="darkblue", fg="white")
         b1_1.place(x=490,y=307,width=215,height=40) 
 
 #Attendance face button      
@@ -162,6 +163,10 @@ class Face_Recognition_System:
     def train_data(self):
         self.new_window=Toplevel(self.root)
         self.app=Train(self.new_window)    
+
+    def face_data(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Face_Recognition(self.new_window)        
  
 
    
