801a20d11a5371874558c44afa609c0c1fc3a919
anjalijha301@gmail.com <anjalijha301@gmail.com>
2023-04-28 17:08:47 +0530

    Designing of train.py

--- train.py
+++ train.py
@@ -13,30 +13,31 @@ class Train:
         self.root=root
         self.root.geometry("1530x790+0+0")
         self.root.title("face Recognition System")
+        self.root.state('zoomed')
 
-        title_lb1=Label(self.root,text="TRAIN DATA SET",font=("times new romain",35,"bold"),bg="white",fg="red")
-        title_lb1.place(x=0, y=0, width=1530,height=45)
+        title_lb1=Label(self.root,text="TRAIN DATA SET",font=("times new romain",35,"bold"),bg="dark violet",fg="white")
+        title_lb1.place(x=0, y=0, width=1540,height=45)
 
 #========================Top Image ======================
         img_top = Image.open("college_images/facialrecognition.png ")
-        img_top=img_top.resize((1530, 325),Image.Resampling.LANCZOS)
+        img_top=img_top.resize((1540, 335),Image.Resampling.LANCZOS)
         self.photoimg_top=ImageTk.PhotoImage(img_top)
 
         f_lbl=Label(self.root,image=self.photoimg_top)
-        f_lbl.place(x=0,y=55,width=1530,height=325) 
+        f_lbl.place(x=0,y=45,width=1540,height=335) 
 
         #================Button=============
 
-        b1_1=Button(self.root,text="TRAIN DATA",command=self.train_classifier, cursor="hand2", font=("times new roman",30,"bold"),bg="red", fg="white")
-        b1_1.place(x=0,y=380,width=1530,height=60) 
+        b1_1=Button(self.root,text="TRAIN DATA",command=self.train_classifier, cursor="hand2", font=("times new roman",35,"bold"),bg="dark violet", fg="white")
+        b1_1.place(x=0,y=380,width=1540,height=60) 
 
 #=========================Bottom Images =================
         img_bottom = Image.open("college_images/public.jpg")
-        img_bottom=img_bottom.resize((1530, 325),Image.Resampling.LANCZOS)
+        img_bottom=img_bottom.resize((1540, 350),Image.Resampling.LANCZOS)
         self.photoimg_bottom=ImageTk.PhotoImage(img_bottom)
 
         f_lbl=Label(self.root,image=self.photoimg_bottom)
-        f_lbl.place(x=0,y=440,width=1530,height=325)
+        f_lbl.place(x=0,y=440,width=1540,height=350)
 
 #===================Functioin for train image==================
   
