f6ec261243543512af9ffbcc15404b826c6dcc19
anjalijha301@gmail.com <anjalijha301@gmail.com>
2023-04-29 01:27:55 +0530

    Design of developer.py

--- developer.py
+++ developer.py
@@ -24,18 +24,18 @@ class Developer:
         f_lbl=Label(self.root,image=self.photoimg_top)
         f_lbl.place(x=0,y=45,width=1540,height=750)
 #Frame.
-        main_frame = Frame( f_lbl, bd =2,bg="white")
-        main_frame.place(x=1000,y=0,width=500,height=600) 
+        main_frame = Frame(f_lbl, bd =2,bg="white")
+        main_frame.place(x=1000,y=0,width=540,height=750) 
 
-        img_top1 = Image.open("college_images/kiran.jpg ")
-        img_top1=img_top1.resize((200, 200),Image.Resampling.LANCZOS)
-        self.photoimg_top1=ImageTk.PhotoImage(img_top1)
+        # img_top1 = Image.open("college_images/kiran.jpg ")
+        # img_top1=img_top1.resize((200, 200),Image.Resampling.LANCZOS)
+        # self.photoimg_top1=ImageTk.PhotoImage(img_top1)
 
-        f_lbl=Label(main_frame,image=self.photoimg_top1)
-        f_lbl.place(x=300,y=0,width=200,height=200) 
+        # f_lbl=Label(main_frame,image=self.photoimg_top1)
+        # f_lbl.place(x=300,y=0,width=200,height=200) 
 
  #Developer info       
-        dev_label=Label(main_frame,text="Hello my name ,kiran",font=("times new romain",20 ,"bold"),bg="white")
+        dev_label=Label(main_frame,text="Hello my name ,Anjali",font=("times new romain",20 ,"bold"),bg="white")
         dev_label.place(x=0,y=5)
 
         dev_label=Label(main_frame,text="I am full stack developer",font=("times new romain",20 ,"bold"),bg="white")
@@ -46,7 +46,7 @@ class Developer:
         self.photoimg2=ImageTk.PhotoImage(img2)
 
         f_lbl=Label(main_frame,image=self.photoimg2)
-        f_lbl.place(x=0,y=210,width=500,height=390) 
+        f_lbl.place(x=330,y=0,width=200,height=200) 
 
 
 
