d22803e078d4a173cdde4df736c9172dfcbbe725
anjalijha301@gmail.com <anjalijha301@gmail.com>
2023-04-29 01:29:30 +0530

    photo change in image.py

--- student.py
+++ student.py
@@ -73,33 +73,33 @@ class Student:
             self.var_id.set(id+1)
         #first image
         img = Image.open("college_images/face-recognition.png ")
-        img=img.resize((500, 130),Image.Resampling.LANCZOS)
+        img=img.resize((530, 130),Image.Resampling.LANCZOS)
         self.photoimg=ImageTk.PhotoImage(img)
 
         f_lbl=Label(self.root,image=self.photoimg)
-        f_lbl.place(x=0,y=0,width=500,height=130)
+        f_lbl.place(x=0,y=0,width=530,height=130)
 #second image
-        img1 = Image.open("college_images/smart-attendance.jpg")
-        img1=img1.resize((500, 130),Image.Resampling.LANCZOS)
+        img1 = Image.open("Images/student_2.jpg")
+        img1=img1.resize((530, 130),Image.Resampling.LANCZOS)
         self.photoimg1=ImageTk.PhotoImage(img1)
 
         f_lbl=Label(self.root,image=self.photoimg1)
-        f_lbl.place(x=500,y=0,width=500,height=130) 
+        f_lbl.place(x=500,y=0,width=530,height=130) 
 #Third image
-        img2 = Image.open("college_images/classstudent.jpg ")
-        img2=img2.resize((500, 130),Image.Resampling.LANCZOS)
+        img2 = Image.open("Images/student_3.jpg")
+        img2=img2.resize((540, 130),Image.Resampling.LANCZOS)
         self.photoimg2=ImageTk.PhotoImage(img2)
 
         f_lbl=Label(self.root,image=self.photoimg2)
-        f_lbl.place(x=1000,y=0,width=550,height=130) 
+        f_lbl.place(x=1000,y=0,width=540,height=130) 
 
         #background image       
         img3 = Image.open("college_images/wp2551980.jpg ")
-        img3=img3.resize((1530, 710),Image.Resampling.LANCZOS)
+        img3=img3.resize((1430, 710),Image.Resampling.LANCZOS)
         self.photoimg3=ImageTk.PhotoImage(img3)
 
         bg_img=Label(self.root,image=self.photoimg3)
-        bg_img.place(x=0,y=130,width=1530,height=710) 
+        bg_img.place(x=0,y=130,width=1540,height=710) 
 
         title_lb1=Label(bg_img,text="STUDENT MANAGEMENT SYSTEM",font=("times new romain",35,"bold"),bg="dark violet",fg="white")
         title_lb1.place(x=0, y=0, width=1530,height=45)
@@ -111,12 +111,12 @@ class Student:
         Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="Student Details")
         Left_frame.place(x=0,y=10,width=700, height=590)
 
-        img_left = Image.open("college_images/AdobeStock_303989091.jpeg ")
+        img_left = Image.open("Images/student_4.jpg")
         img_left=img_left.resize((700, 130),Image.Resampling.LANCZOS)
         self.photoimg_left=ImageTk.PhotoImage(img_left)
 
         f_lbl=Label(Left_frame,image=self.photoimg_left)
-        f_lbl.place(x=0,y=0,width=745,height=130)
+        f_lbl.place(x=5,y=0,width=690,height=130)
 
 #current course
         current_course_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="Current Course details")
