f1b5bf47630f786ab753abb62032f6b8f634b535
anjalijha301@gmail.com <anjalijha301@gmail.com>
2023-04-28 17:01:34 +0530

    designing of Help.py

--- help.py
+++ help.py
@@ -11,17 +11,18 @@ class Help:
         self.root=root
         self.root.geometry("1530x790+0+0")
         self.root.title("face Recognition System")
+        self.root.state('zoomed')
 
-        title_lb1=Label(self.root,text="HELP DESK",font=("times new romain",35,"bold"),bg="white",fg="red")
-        title_lb1.place(x=0, y=0, width=1530,height=45)
+        title_lb1=Label(self.root,text="HELP DESK",font=("times new romain",35,"bold"),bg="dark violet",fg="white")
+        title_lb1.place(x=0, y=0, width=1540,height=45)
 
 #========================Top Image ======================
         img_top = Image.open("college_images/h.jpeg")
-        img_top=img_top.resize((1530, 720),Image.Resampling.LANCZOS)
+        img_top=img_top.resize((1540, 750),Image.Resampling.LANCZOS)
         self.photoimg_top=ImageTk.PhotoImage(img_top)
 
         f_lbl=Label(self.root,image=self.photoimg_top)
-        f_lbl.place(x=0,y=55,width=1530,height=720)
+        f_lbl.place(x=0,y=45,width=1540,height=750)
 
         dev_label=Label(f_lbl,text="Email:anjalijha301@gmail.com",font=("times new romain",20 ,"bold"),bg="white")
         dev_label.place(x=550,y=220)
